77bbad22de095ff7a2cd5ab834e7619c12ce6f43
Samuel Briceno <sambricep@gmail.com>
2024-02-03 22:05:28 -0500

    Agrego cambios en el codigo de readme para corregir los errores

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -6,17 +6,18 @@ int main(int argc, char *argv[]) {
      fprintf(stderr,"Usage: program <number>\n");
         return 1;
     }
-    
-    int number = atoi(argv[1]); //Convert the console input argument to an integer
-    
+
+    int number = atoi(argv[1]);
+    //Convert the console input argument to an integer
+
     /*It must be verified that the entered number is greater than or equal to zero*/
     if(number < 0)
     {
-        fprintf(stderr,"The number must be greater than or equal to zero\n");
+        fprintf("The number must be greater than or equal to zero\n");
         return(1);
     }
     
-    /*Implement the function that calculates the factorial of the input argument*/
+/*Implement the function that calculates the factorial of the input argument*/
     int factorial = 1;
     int b;
     for (b = number; b > 1; b--)
@@ -26,10 +27,10 @@ int main(int argc, char *argv[]) {
     
     /*Print the result*/
     fprintf(stdout,"The factorial of %d is: %d\n",number,factorial);
-    
+
     /*Print the result*/
     
-    /*Implement the function that calculates if the argument is a prime number*/
+/*Implement the function that calculates if the argument is a prime number*/
     int count = 0,index = 1;
     while (index < number)
     {
@@ -51,6 +52,6 @@ int main(int argc, char *argv[]) {
     }
 
     /*Print the result*/
-    
-    return 0;
-}
+
+    return 0;
+    }
