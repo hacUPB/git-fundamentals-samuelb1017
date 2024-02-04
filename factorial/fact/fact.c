7687a71652fd6b92bfddec20b69f63ef7310983a
Samuel Briceno <sambricep@gmail.com>
2024-02-03 21:45:44 -0500

    Realizo cambios debido a que los errores no fueron correctamente resueltos

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -7,16 +7,29 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     
-    int number = atoi(argv[1]);//Convert the console imput argument to an integer
+    int number = atoi(argv[1]); //Convert the console input argument to an integer
     
     /*It must be verified that the entered number is greater than or equal to zero*/
     if(number < 0)
     {
-        fprintf(stderr"The number must be greater than or equal to zero");
+        fprintf(stderr,"The number must be greater than or equal to zero\n");
         return(1);
     }
-
+    
     /*Implement the function that calculates the factorial of the input argument*/
+    int factorial = 1;
+    int b;
+    for (b = number; b > 1; b--)
+    {
+        factorial *= b;
+    }
+    
+    /*Print the result*/
+    fprintf(stdout,"The factorial of %d is: %d\n",number,factorial);
+    
+    /*Print the result*/
+    
+    /*Implement the function that calculates if the argument is a prime number*/
     int count = 0,index = 1;
     while (index < number)
     {
@@ -27,7 +40,7 @@ int main(int argc, char *argv[]) {
         index++;
     }
     
-    /Print the result/
+    /*Print the result*/
     if(count == 1)
     {
         printf("%d is a prime number\n",number);
@@ -37,7 +50,7 @@ int main(int argc, char *argv[]) {
         printf("%d is not a prime number\n",number);
     }
 
-    /Print the result/
+    /*Print the result*/
     
     return 0;
 }
